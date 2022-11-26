966b83db4f3c5c03919e6d5a8f02e0bac87633d5
khatuntsevke <kirill.khatuntsev@yandex.ru>
2022-11-27 01:18:56 +0500

    to mentor

---
+++ ConsoleApp/Planet.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.IO;
+using System.Numerics;
+using System.Security.Cryptography.X509Certificates;
+
+class Planet
+{    
+    public string Name { get; set; }
+    public int SerialNumber { get; set; }
+    public int EquatorLength { get; set; }
+    public Planet? Previous { get; set; }
+}
+
+class PlanetCatalog
+{
+    int _requestCounter;
+    List<Planet> _body;
+
+    public PlanetCatalog()
+    {
+        _requestCounter = 0;
+
+        Planet venus = new Planet();
+        venus.Name = "Венера";
+        venus.SerialNumber = 2;
+        venus.EquatorLength = 38025;
+        venus.Previous = null;
+
+        Planet earth = new Planet();
+        earth.Name = "Земля";
+        earth.SerialNumber = 3;
+        earth.EquatorLength = 40075;
+        earth.Previous = earth;
+
+        Planet mars = new Planet();
+        mars.Name = "Марс";
+        mars.SerialNumber = 4;
+        mars.EquatorLength = 21343;
+        mars.Previous = earth;
+
+        _body = new List<Planet>(3);
+        _body.Add(venus);
+        _body.Add(earth);
+        _body.Add(mars);
+    }
+
+    public (int serialNumber, int equatorLength, string? errorMsg) GetPlanet(string targetName)
+    {
+        _requestCounter++;
+        string? errorMsg = null;
+        if (_requestCounter == 3)
+        {
+            errorMsg = "Вы спрашиваете слишком часто";
+            _requestCounter = 0;
+        }
+        
+        if (!string.IsNullOrEmpty(errorMsg))
+        {
+            return (serialNumber: -1, equatorLength: -1, errorMsg: errorMsg);
+        }
+
+        foreach (var planet in _body)
+        {
+            if (planet.Name == targetName)
+            {                
+                return (serialNumber: planet.SerialNumber, equatorLength: planet.EquatorLength, errorMsg: errorMsg);
+            }
+        }
+        return (serialNumber: -1, equatorLength: -1, errorMsg: "Не удалось найти планету");
+    }
+
+    public (int serialNumber, int equatorLength, string? errorMsg) GetPlanetWithValidator(string targetName, Func<string, string?> PlanetValidator)
+    {
+        string? errorMsg = PlanetValidator(targetName);
+        
+        if (!string.IsNullOrEmpty(errorMsg))
+        {
+            return (serialNumber: -1, equatorLength: -1, errorMsg: errorMsg);
+        }
+
+        foreach (var planet in _body)
+        {
+            if (planet.Name == targetName)
+            {
+                return (serialNumber: planet.SerialNumber, equatorLength: planet.EquatorLength, errorMsg: errorMsg);
+            }
+        }
+        return (serialNumber: -1, equatorLength: -1, errorMsg: "Не удалось найти планету");
+    }
+}
--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,2 +1,120 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Program1();
+Program2();
+Program3();
+Program3WithStar();
+
+void Program1()
+{
+    var venus = new
+    {
+        nameOfThePlanet = "Венера",
+        serialNumberFromTheSun = 2,
+        equatorLength = 38025,
+        previousPlanet = null as object,
+    };
+
+    var earth = new
+    {
+        nameOfThePlanet = "Земля",
+        serialNumberFromTheSun = 3,
+        equatorLength = 40075,
+        previousPlanet = venus as object,
+    };
+
+    var mars = new
+    {
+        nameOfThePlanet = "Марс",
+        serialNumberFromTheSun = 4,
+        equatorLength = 21343,
+        previousPlanet = earth as object,
+    };
+
+    var venus2 = new
+    {
+        nameOfThePlanet = "Венера",
+        serialNumberFromTheSun = 2,
+        equatorLength = 38025,
+        previousPlanet = null as object,
+    };
+
+    var eq = venus2 == venus;
+
+    Console.WriteLine("=== Программа 1 ===");
+    Console.WriteLine($"{venus}\nIsEqualVenus = {venus.Equals(venus)}");
+    Console.WriteLine($"{earth}\nIsEqualVenus = {earth.Equals(venus)}");
+    Console.WriteLine($"{mars}\nIsEqualVenus = {mars.Equals(venus)}");
+    Console.WriteLine($"{venus2}\nIsEqualVenus = {venus2.Equals(venus)}");    
+}
+
+void Program2()
+{
+    Console.WriteLine("\n=== Программа 2 ===");
+    
+    var pCatalog = new PlanetCatalog();
+    var names = new List<string> { "Земля", "Лимония", "Марс" };
+
+    foreach(string nameOfPlanet in names)
+    {
+        var (serialNumber, equatorLength, errorMsg) = pCatalog.GetPlanet(nameOfPlanet);
+        Console.WriteLine($"Планета - {nameOfPlanet}\n" +
+            $"Порядковый номер от Солнца - {serialNumber}\n" +
+            $"Длина экватора - {equatorLength}\n" +
+            $"Ошибка при запросе в каталог - {errorMsg}\n");
+    }
+}
+
+void Program3()
+{
+    Console.WriteLine("\n=== Программа 3 ===");
+
+    var pCatalog = new PlanetCatalog();
+    var names = new List<string> { "Земля", "Лимония", "Марс" };
+
+    int counter = 0;
+
+    Func<string, string> lambda = (string nameOfPlanet) =>
+    {
+        counter++;
+        if (counter == 3)
+        {
+            return "Вы спрашиваете слишком часто";
+        }
+        return "";
+    };
+
+    foreach (string nameOfPlanet in names)
+    {
+        var (serialNumber, equatorLength, errorMsg) = pCatalog.GetPlanetWithValidator(nameOfPlanet, lambda );
+        Console.WriteLine($"Планета - {nameOfPlanet}\n" +
+            $"Порядковый номер от Солнца - {serialNumber}\n" +
+            $"Длина экватора - {equatorLength}\n" +
+            $"Ошибка при запросе в каталог - {errorMsg}\n");
+    }
+}
+
+void Program3WithStar()
+{
+    Console.WriteLine("\n=== Программа 3* ===");
+
+    var pCatalog = new PlanetCatalog();
+    var names = new List<string> { "Земля", "Лимония", "Марс" };    
+
+    Func<string, string?> lambda = (string nameOfPlanet) =>
+    {        
+        if (nameOfPlanet == "Лимония")
+        {
+            return "Это запретная планета";
+        }
+        return null;
+    };
+
+    foreach (string nameOfPlanet in names)
+    {
+        var (serialNumber, equatorLength, errorMsg) = pCatalog.GetPlanetWithValidator(nameOfPlanet, lambda);
+        Console.WriteLine($"Планета - {nameOfPlanet}\n" +
+            $"Порядковый номер от Солнца - {serialNumber}\n" +
+            $"Длина экватора - {equatorLength}\n" +
+            $"Ошибка при запросе в каталог - {errorMsg}\n");
+    }
+}
+
